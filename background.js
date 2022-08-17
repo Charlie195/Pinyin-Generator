86cc67bdc0690c38885ce4b745a787ed30546724
Charlie Li <charlie.li711935@gmail.com>
2022-08-17 14:22:46 -0400

    transliterate button outputs correct pinyin

--- background.js
+++ background.js
@@ -1,26 +1,31 @@
 // execute receiver function when message from content.js is received
-chrome.runtime.onMessage.addListener(receiver);
+chrome.runtime.onMessage.addListener(receivedCharacterMsg);
 
 // global selectedCharacter
 window.selectedCharacter = "";
 
-function receiver(request, sender, sendResponse) {
+function receivedCharacterMsg(request, sender, sendResponse) {
     // set selectedCharacter
     window.selectedCharacter = request.text;
 
+    console.log(selectedCharacter);
     // find the pinyin for the selectedCharacter
-    findPinyin(window.selectedCharacter);
+    findPinyin(window.selectedCharacter, request.finishedTransliteration);
 }
 
-function findPinyin(character) {
+function findPinyin(character, finished) {
     // find index of selectedCharacter in data source
     var index = characterSource.findIndex(item => item.Character === character);
 
     // find and output corresponding pinyin
     try {
         const pinyin = characterSource[index]["Pinyin"];
-        console.log(pinyin);
+        chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
+            chrome.tabs.sendMessage(tabs[0].id, {text: pinyin, finishedTransliteration: finished});
+        });
     } catch(err) {
-        console.log("There is no pinyin for: " + character);
+        chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
+            chrome.tabs.sendMessage(tabs[0].id, {text: character, finishedTransliteration: finished});
+        });
     }
 }
--- content.js
+++ content.js
@@ -8,6 +8,10 @@ console.log("button should be loaded");
 transliterateBtn.onclick = receiveText;
 
 var title;
+var transliteratedPinyin = "";
+
+// Listener to receive the background.js message with pinyin
+chrome.runtime.onMessage.addListener(receivedPinyinMsg);
 
 function receiveText() {
     // Title
@@ -15,15 +19,32 @@ function receiveText() {
 
     // Transliterate through each character
     for (var i = 0; i < title.length; i++) {
-        transliterate(title[i]);
+        transliterate(title[i], i == title.length - 1);
     }
+    // transliterate("的");
+
+    // displayPinyin(transliteratedPinyin);
 }
 
-function transliterate(character) {
+function transliterate(character, process) {
     // send text as message object to background.js
     let message = {
-        text: character
+        text: character,
+        finishedTransliteration: process
     };
 
     chrome.runtime.sendMessage(message);
+}
+
+// Receiving the pinyin message as an event object
+function receivedPinyinMsg(request) {
+    transliteratedPinyin += request.text + " "; // Setting keywords as favTeams from popup.js
+
+    if (request.finishedTransliteration) {
+        displayPinyin(transliteratedPinyin);
+    }
+}
+
+function displayPinyin(displayText) {
+    console.log(displayText);
 }
