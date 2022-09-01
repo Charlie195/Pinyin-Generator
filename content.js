272076f5d10b87c1f881e3311a4fda8410fd1abf
Charlie Li <charlie.li711935@gmail.com>
2022-09-01 13:33:21 -0400

    window created

--- content.js
+++ content.js
@@ -1,107 +1,254 @@
-// Listener to receive the popup.js message with extension state
-chrome.runtime.onMessage.addListener(receiveExtensionStateMsg);
+window.onload = init;
 
-// Variable to ensure the initTranslation isn't executed multiple times over
-var initiated;
+function init() {
+    // Setting up activate button
+    const activateBtn = document.createElement("button");
+    activateBtn.setAttribute("id", "activateBtn");
+    activateBtn.innerHTML = "TRANSLITERATE";
+    document.getElementById("meta_content").appendChild(activateBtn);
 
-// Global variable to store the function reference
-var receiveText;
-var receivePinyinMsg;
+    var deactivateBtn = document.createElement("button");
+    deactivateBtn.setAttribute("id", "deactivateBtn");
+    deactivateBtn.innerHTML = "DEACTIVATE";
+    document.getElementById("meta_content").appendChild(deactivateBtn);
+    deactivateBtn.hidden = true;
 
-// Original element of the text on the page
-var origTextElement;
+    // Variable to ensure the initTranslation isn't executed multiple times over
+    var initiated;
 
-// String to store transliterated pinyin
-var transliteratedPinyin = "";
+    // Global variable to store the function reference
+    var receiveText;
+    var receivePinyinMsg;
 
-if (localStorage.getItem("extension started") == "true" && !initiated) { // Local storage doesn't store actual booleans but rather strings
-    initTranslation();
-}
+    // Original element of the text on the page
+    var origTextElement;
 
-function receiveExtensionStateMsg(extensionStateMsg) {
-    if (extensionStateMsg["id"] == "extensionStateMsg") {
-        if (extensionStateMsg["started"]) {
-            localStorage.setItem("extension started", true);
-            if (!initiated) {
-                initTranslation();
+    // String to store transliterated pinyin
+    var transliteratedPinyin = "";
+
+    activateBtn.onclick = activateExtension;
+
+    function activateExtension() {
+        var extensionWindow = document.createElement("div");
+        extensionWindow.setAttribute("id", "extensionWindowDiv");
+        extensionWindow.innerHTML = `
+            <div id="draggableHeader">Click here to move</div>
+            <p>Move</p>
+            <p>this</p>
+            <p>DIV</p>
+        `;
+
+        var container = document.querySelector("html");
+        container.insertBefore(extensionWindow, container.firstChild);
+
+        var cssElement = document.createElement("link");
+        cssElement.setAttribute("rel", "stylesheet");
+        cssElement.setAttribute("href", "extensionWindow.css");
+        document.querySelector("html").appendChild(cssElement);
+
+        // Make the DIV element draggable:
+        dragElement(document.getElementById("extensionWindowDiv"));
+
+        function dragElement(elmnt) {
+            var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+            if (document.getElementById("draggableHeader")) {
+                // if present, the header is where you move the DIV from:
+                document.getElementById("draggableHeader").onmousedown = dragMouseDown;
+            } else {
+                // otherwise, move the DIV from anywhere inside the DIV:
+                elmnt.onmousedown = dragMouseDown;
+            }
+
+            function dragMouseDown(e) {
+                e = e || window.event;
+                e.preventDefault();
+                // get the mouse cursor position at startup:
+                pos3 = e.clientX;
+                pos4 = e.clientY;
+                document.onmouseup = closeDragElement;
+                // call a function whenever the cursor moves:
+                document.onmousemove = elementDrag;
+            }
+
+            function elementDrag(e) {
+                e = e || window.event;
+                e.preventDefault();
+                // calculate the new cursor position:
+                pos1 = pos3 - e.clientX;
+                pos2 = pos4 - e.clientY;
+                pos3 = e.clientX;
+                pos4 = e.clientY;
+                // set the element's new position:
+                elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+                elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+            }
+
+            function closeDragElement() {
+                // stop moving when mouse button is released:
+                document.onmouseup = null;
+                document.onmousemove = null;
             }
         }
-        else {
-            localStorage.setItem("extension started", false);
-            endTranslation();
-        }
-    }
-}
-
-function initTranslation() {
-    receiveText = (eventObj) => {
-        // Getting highlighted text before possible DOM modifications that would unhighlight the text
-        var selectedText = window.getSelection().toString();
         
-        if (origTextElement) { // Delete the previous displayed transliteration if there is one
+        receiveText = (eventObj) => {
+            // Getting highlighted text before possible DOM modifications that would unhighlight the text
+            var selectedText = window.getSelection().toString();
+            
+            if (origTextElement) { // Delete the previous displayed transliteration if there is one
+                origTextElement.innerHTML = origTextElement.innerHTML.replace(`<br>${transliteratedPinyin}`, ""); // We need to set the innerHTML to the changed value
+            }
+
+            transliteratedPinyin = ""; // Emptying the transliterated pinyin storage
+            origTextElement = eventObj.target;
+
+            // Transliterate through each character of highlighted text
+            for (var i = 0; i < selectedText.length; i++) {
+                transliterate(selectedText[i], i == selectedText.length - 1);
+            }
+        }
+
+        // Receiving the pinyin message as an event object
+        receivePinyinMsg = (pinyinMsg) => {
+            if (pinyinMsg["id"] == "pinyinMsg") {
+                transliteratedPinyin += pinyinMsg["text"] + " "; // Setting keywords as favTeams from popup.js
+
+                if (pinyinMsg["finishedTransliteration"]) { // Send the pinyin to the popup to display when all characters have been transliterated
+                    displayPinyin();
+                }
+            }
+        }
+
+        // Execute receiveText when mouse is released
+        window.addEventListener("mouseup", receiveText);
+
+        // Listener to receive the background.js message with pinyin
+        chrome.runtime.onMessage.addListener(receivePinyinMsg);
+
+        function transliterate(character, process) {
+            // Send text as message object to background.js
+            var characterMsg = {
+                "id": "characterMsg",
+                "text": character,
+                "finishedTransliteration": process // Identifying if there are more characters to transliterate
+            };
+
+            chrome.runtime.sendMessage(characterMsg);
+        }
+
+        function displayPinyin() {
+            origTextElement.innerHTML += "<br>" + transliteratedPinyin;
+        }
+
+        var activateBtn = document.getElementById("activateBtn");
+        activateBtn.hidden = true;
+
+        var deactivateBtn = document.getElementById("deactivateBtn");
+        deactivateBtn.hidden = false;
+
+        deactivateBtn.onclick = deactivateExtension;
+    }
+
+    function deactivateExtension() {
+        window.removeEventListener("mouseup", receiveText);
+
+        chrome.runtime.onMessage.removeListener(receivePinyinMsg);
+
+        if (origTextElement) { // Delete the last displayed transliteration
             origTextElement.innerHTML = origTextElement.innerHTML.replace(`<br>${transliteratedPinyin}`, ""); // We need to set the innerHTML to the changed value
         }
+        
+        var activateBtn = document.getElementById("activateBtn");
+        activateBtn.hidden = false;
 
-        transliteratedPinyin = ""; // Emptying the transliterated pinyin storage
-        origTextElement = eventObj.target;
-
-        // Transliterate through each character of highlighted text
-        for (var i = 0; i < selectedText.length; i++) {
-            transliterate(selectedText[i], i == selectedText.length - 1);
-        }
+        var deactivateBtn = document.getElementById("deactivateBtn");
+        deactivateBtn.hidden = true;
     }
 
-    // Receiving the pinyin message as an event object
-    receivePinyinMsg = (pinyinMsg) => {
-        if (pinyinMsg["id"] == "pinyinMsg") {
-            transliteratedPinyin += pinyinMsg["text"] + " "; // Setting keywords as favTeams from popup.js
+    // function receiveExtensionStateMsg(extensionStateMsg) {
+    //     if (extensionStateMsg["id"] == "extensionStateMsg") {
+    //         if (extensionStateMsg["started"]) {
+    //             localStorage.setItem("extension started", true);
+    //             if (!initiated) {
+    //                 initTranslation();
+    //             }
+    //         }
+    //         else {
+    //             localStorage.setItem("extension started", false);
+    //             endTranslation();
+    //         }
+    //     }
+    // }
 
-            if (pinyinMsg["finishedTransliteration"]) { // Send the pinyin to the popup to display when all characters have been transliterated
-                displayPinyin();
-            }
-        }
-    }
+    // function initTranslation() {
+    //     receiveText = (eventObj) => {
+    //         // Getting highlighted text before possible DOM modifications that would unhighlight the text
+    //         var selectedText = window.getSelection().toString();
+            
+    //         if (origTextElement) { // Delete the previous displayed transliteration if there is one
+    //             origTextElement.innerHTML = origTextElement.innerHTML.replace(`<br>${transliteratedPinyin}`, ""); // We need to set the innerHTML to the changed value
+    //         }
 
-    // Execute receiveText when mouse is released
-    window.addEventListener("mouseup", receiveText);
+    //         transliteratedPinyin = ""; // Emptying the transliterated pinyin storage
+    //         origTextElement = eventObj.target;
 
-    // Listener to receive the background.js message with pinyin
-    chrome.runtime.onMessage.addListener(receivePinyinMsg);
+    //         // Transliterate through each character of highlighted text
+    //         for (var i = 0; i < selectedText.length; i++) {
+    //             transliterate(selectedText[i], i == selectedText.length - 1);
+    //         }
+    //     }
 
-    function transliterate(character, process) {
-        // Send text as message object to background.js
-        var characterMsg = {
-            "id": "characterMsg",
-            "text": character,
-            "finishedTransliteration": process // Identifying if there are more characters to transliterate
-        };
+    //     // Receiving the pinyin message as an event object
+    //     receivePinyinMsg = (pinyinMsg) => {
+    //         if (pinyinMsg["id"] == "pinyinMsg") {
+    //             transliteratedPinyin += pinyinMsg["text"] + " "; // Setting keywords as favTeams from popup.js
 
-        chrome.runtime.sendMessage(characterMsg);
-    }
+    //             if (pinyinMsg["finishedTransliteration"]) { // Send the pinyin to the popup to display when all characters have been transliterated
+    //                 displayPinyin();
+    //             }
+    //         }
+    //     }
 
-    function displayPinyin() {
-        origTextElement.innerHTML += "<br>" + transliteratedPinyin;
+    //     // Execute receiveText when mouse is released
+    //     window.addEventListener("mouseup", receiveText);
 
-        // Send display transliteration message to popup.js
-        var displayMsg = {
-            "id": "displayMsg",
-            "text": transliteratedPinyin
-        };
+    //     // Listener to receive the background.js message with pinyin
+    //     chrome.runtime.onMessage.addListener(receivePinyinMsg);
 
-        chrome.runtime.sendMessage(displayMsg);
-    }
+    //     function transliterate(character, process) {
+    //         // Send text as message object to background.js
+    //         var characterMsg = {
+    //             "id": "characterMsg",
+    //             "text": character,
+    //             "finishedTransliteration": process // Identifying if there are more characters to transliterate
+    //         };
 
-    initiated = true;
-}
+    //         chrome.runtime.sendMessage(characterMsg);
+    //     }
 
-function endTranslation() {
-    window.removeEventListener("mouseup", receiveText);
+    //     function displayPinyin() {
+    //         origTextElement.innerHTML += "<br>" + transliteratedPinyin;
 
-    chrome.runtime.onMessage.removeListener(receivePinyinMsg);
+    //         // Send display transliteration message to popup.js
+    //         var displayMsg = {
+    //             "id": "displayMsg",
+    //             "text": transliteratedPinyin
+    //         };
 
-    if (origTextElement) { // Delete the last displayed transliteration
-        origTextElement.innerHTML = origTextElement.innerHTML.replace(`<br>${transliteratedPinyin}`, ""); // We need to set the innerHTML to the changed value
-    }
+    //         chrome.runtime.sendMessage(displayMsg);
+    //     }
 
-    initiated = false;
+    //     initiated = true;
+    // }
+
+    // function endTranslation() {
+    //     window.removeEventListener("mouseup", receiveText);
+
+    //     chrome.runtime.onMessage.removeListener(receivePinyinMsg);
+
+    //     if (origTextElement) { // Delete the last displayed transliteration
+    //         origTextElement.innerHTML = origTextElement.innerHTML.replace(`<br>${transliteratedPinyin}`, ""); // We need to set the innerHTML to the changed value
+    //     }
+
+    //     initiated = false;
+    // }
 }
